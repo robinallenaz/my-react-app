72e1e3cf07c8175608ab79c141c552da625ecee8
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-10 15:34:15 -0700

    Hard coded image path while attempting to figure out how to render different images with {image} correctly

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
 function Article({ title, date, image, text }) {
   return (
     <article>
       <h2>{title}</h2>
       <time datetime={date}>{date}</time>
       <br />
-      <img src={image} alt={title} />
+      <img src={"/blog-image-1.jpg"} alt={title} />
       <p>{text}</p>
       <a>Continues...</a>
     </article>
